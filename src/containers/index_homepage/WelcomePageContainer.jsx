51046aaa1c51e1936ea267cffd988e108115880c
xuyangdong <804954666@qq.com>
2017-02-26 22:07:18 +0800

    temp

--- src/containers/index_homepage/WelcomePageContainer.jsx
+++ src/containers/index_homepage/WelcomePageContainer.jsx
@@ -3,22 +3,22 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import {ROLE_PATRIARCH,ROLE_TEACHER,ROLE_STUDENT} from '../../constant'
 import TeacherWelcomePage from './TeacherWelcomePage'
+import StudentWelcomePage from './StudentWelcomePage'
 
 const WelcomPageContainer = React.createClass({
   render(){
-    console.log("--->:",this.props.user.get('userInfo'))
     switch (this.props.user.get('userInfo').userStyle) {
       case ROLE_PATRIARCH:
-        return <div>官员</div>
+        return (<div>暂无官员</div>)
         break;
       case ROLE_TEACHER:
         return <TeacherWelcomePage />
         break;
       case ROLE_STUDENT:
-        return <div>学生</div>
+        return <StudentWelcomePage />
         break;
       default:
-        return <div>错误</div>
+        return <div>暂无该身份的欢迎页</div>
         break;
     }
   }
